bbcc04e298899d1231017de7a8cd6a992c6cff91
bowendeng666 <bd45@rice.edu>
2021-07-21 22:59:33 +0800

    .

--- app/tasks.py
+++ app/tasks.py
@@ -48,16 +48,4 @@ def export_posts(user_id):
     except:
         _set_task_progress(100)
         app.logger.error('Unhandled exception', exc_info=sys.exc_info())
-    finally:
 
-def example(seconds):
-    job = get_current_job()
-    print('Starting task')
-    for i in range(seconds):
-        job.meta['progress'] = 100.0 * i / seconds
-        job.save_meta()
-        print(i)
-        time.sleep(1)
-    job.meta['progress'] = 100
-    job.save_meta()
-    print('Task completed')
